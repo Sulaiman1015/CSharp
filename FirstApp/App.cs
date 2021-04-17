18c2b3eba2fd54d847426d0a1e7f7b75b8a91549
Sulaiman1015 <Sulaiman1015@gmail.com>
2021-04-17 13:55:59 +0200

    Add files
    C#

---
+++ FirstApp/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
